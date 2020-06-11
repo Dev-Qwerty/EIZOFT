933b401a096d09e6841f8d806e58c4a061259791
Albin Jose <albinavirappatt@gmail.com>
2020-06-11 22:41:15 +0530

    style(home):add typewriter effect

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,22 +1,53 @@
 <template>
     <div class="container">
-        <p>Together We Create</p>
+        <div class="title">
+            <p>Together We 
+            <vue-typer
+                :text='["Code.","Create.","Build."]'
+                :repeat='Infinity'
+                :shuffle='false'
+                initial-action='typing'
+                :pre-type-delay='500'
+                :type-delay='120'
+                :pre-erase-delay='2000'
+                :erase-delay='120'
+                erase-style='backspace'
+                :erase-on-complete='false'
+                caret-animation='expand'
+            ></vue-typer>
+        </p>
+        </div>
     </div>
 </template>
 
 
-<style scoped>
+<style>
 .container {
     display: grid;
     justify-content: center;
     margin-top: 19%;
 }
 
-.container > p {
+
+.title > p {
     color: white;
     font-size: 80px;
     margin: 0% auto;
     font-family:'Dancing Script', cursive, Avenir, Helvetica, Arial, sans-serif;
 }
+
+.vue-typer .custom.char.typed {
+    color:white;
+}
+
+/* .vue-typer .custom.caret {
+  display: none;
+} */
+
+.vue-typer .custom.caret {
+  width: 3px;
+  background-color: white;
+}
+
 </style>
 
--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueParticles from 'vue-particles'
+import VueTyperPlugin from 'vue-typer'
+
 Vue.use(VueParticles)
+Vue.use(VueTyperPlugin)
+
 
 Vue.config.productionTip = false
 
